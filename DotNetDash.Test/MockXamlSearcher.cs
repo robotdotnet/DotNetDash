14f7041edd7067bbdbadee436f0b8788e98565cc
Jeremy Koritzinsky <jkoritzinsky@live.com>
2016-01-19 13:26:38 -0600

    Removed unused view converter.

---
+++ DotNetDash.Test/MockXamlSearcher.cs
@@ -0,0 +1,18 @@
+﻿using DotNetDash.BuiltinProcessors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DotNetDash.Test
+{
+    public class MockXamlSearcher : IXamlSearcher
+    {
+        public IEnumerable<Stream> GetXamlDocumentStreams()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
